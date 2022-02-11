7463f3a0d1f4b6465f21182fc124c717937b5350
John Oseni <johnoseni26@gmail.com>
2022-02-11 05:44:38 +0100

    called out the backend to the frontend

--- dist/config/mongoose.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-var _mongoose = _interopRequireDefault(require("mongoose"));
--- dist/public/js/demo/datatables-demo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-// Call the dataTables jQuery plugin
-$(document).ready(function () {
-  $('#dataTable').DataTable();
-});
