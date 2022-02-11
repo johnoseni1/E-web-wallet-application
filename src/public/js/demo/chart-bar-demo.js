f88ef45e33c0f827fcc60f058c1b06822b2d8fd7
John Oseni <johnoseni26@gmail.com>
2022-02-11 06:03:14 +0100

    fixing the account.js routes in the codes

--- src/public/js/demo/chart-bar-demo.js
+++ src/public/js/demo/chart-bar-demo.js
@@ -35,9 +35,9 @@ var myBarChart = new Chart(ctx, {
     labels: ["January", "February", "March", "April", "May", "June"],
     datasets: [{
       label: "Revenue",
-      backgroundColor: "#4e73df",
+      backgroundColor: "#fd7e14",
       hoverBackgroundColor: "#2e59d9",
-      borderColor: "#4e73df",
+      borderColor: "#fd7e14",
       data: [4215, 5312, 6251, 7841, 9821, 14984],
     }],
   },
